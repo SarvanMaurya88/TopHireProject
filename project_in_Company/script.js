50add07d781f239b442f72072f83329094d5e7b3
SarvanMaurya88 <139687292+SarvanMaurya88@users.noreply.github.com>
2023-08-03 19:42:56 +0530

    Add files via upload

---
+++ project_in_Company/script.js
@@ -0,0 +1,84 @@
+let a=document.getElementById("twitter");
+a.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://twitter.com/tophire_co";
+});
+
+let b=document.getElementById("facebook");
+b.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://www.facebook.com/tophire.co/";
+});
+
+let c=document.getElementById("linkedin");
+c.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://www.linkedin.com/company/tophire-co/";
+});
+
+let d=document.getElementById("insta");
+d.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://www.instagram.com/tophire_co/";
+});
+
+let candidate=document.getElementById("candidate");
+candidate.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="#";
+});
+
+let sign=document.getElementById("sign");
+sign.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/candidate/signup";
+});
+
+let invite=document.getElementById("invite");
+invite.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/candidate/signup/referral-program";
+});
+
+let faq=document.getElementById("FAO");
+faq.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/faq/candidate";
+});
+
+let emp=document.getElementById("emp");
+emp.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/employers";
+});
+
+let e_sign=document.getElementById("e-sign");
+e_sign.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/recruiter/signup";
+});
+
+let e_faq=document.getElementById("e-faq");
+e_faq.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.co/faq/employer";
+});
+
+let careers=document.getElementById("careers");
+careers.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://tophire.freshteam.com/jobs";
+});
+
+let prv_policy=document.getElementById("prv-policy");
+prv_policy.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="https://docs.google.com/document/d/e/2PACX-1vSfNZzb0SBfMaYv9guvEZXuzR9fgj8J_lDSmN9maVmCRQewtIZRFl7Wq0qB2hmJNlOf2y8dV_s97U_6/pub";
+});
+
+let img=document.getElementsByClassName("last-img")[0];
+img.addEventListener("click",(e)=>{
+e.preventDefault();
+location.href="hhttps://tophire.co/faq/employer/why-do-i-see-a-codechef-badge-on-certain-candidates";
+});
+
